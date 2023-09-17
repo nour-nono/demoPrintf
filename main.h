b63bfe68e8273ae04f46161145173a25a7785799
ahmedemc2 <ahmedtkitoemc2@gmail.com>
2023-09-17 13:58:04 +0000

    hot fix

--- main.h
+++ main.h
@@ -7,20 +7,51 @@
 #include <stdarg.h>
 
 #define buff_size 1024
+
+/**
+ * _pow - function that return the power of a number
+ *
+ * @a : the base number
+ * @b : the exponent number
+ *
+ * Return: the power of base number
+*/
+
+int _pow(int a, int b)
+{
+	int x = a;
+	while (b > 1)
+	{
+		a *= x;
+		b--;
+	}
+	return (a);
+}
+
 typedef struct placeHolders placeHolders;
 struct placeHolders
 {
     char sp;
     int (*funcPtr)(va_list);
 };
+
 placeHolders specifiers[] =
     {
         {'c', printChar},
-        {'s', printString}
+        {'s', printString},
+	{'d', printInteger},
+	{'i', printInteger},
+	{'u', printUnsigned},
+	{'o', print0ctal},
+	{'x', printHex},
+	{'X', printHEX}
     };
+
 /*first task funcs*/
 int printChar(va_list argu);
 int printString(va_list argu);
 int printPercentage();
+/*second task function*/
+int printInteger(va_list argu);
 
 #endif
