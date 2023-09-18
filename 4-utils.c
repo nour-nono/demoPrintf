2a4886cb5c609aa8dbf95ff9e48dc10132111a9f
nono <nour98nour@gmail.com>
2023-09-18 19:54:24 +0300

    make files betty

--- 4-utils.c
+++ 4-utils.c
@@ -1,28 +1,35 @@
 #include "main.h"
-
+/**
+ * handleBuffer - the function that print the unsigned value
+ *
+ * @a: the number to be printed
+ * @ch: string
+ *
+ * Return: outputLength
+*/
 int handleBuffer(int a, char *ch)
 {
-    static int iForBuffer = 0;
-    int iForInput = 0;
-    static char output[buff_size];
-    static int outputLength = 0;
-    if (a == -1)
-    {
-        write(1, output, iForBuffer);
-        iForBuffer = 0;
-        return (outputLength);
-    }
-    if (a == 1)
-        output[iForBuffer] = *ch, ++outputLength, ++iForBuffer, ++iForInput, --a, output[iForBuffer] = '\0';
-    while (a--)
-    {
-        if (iForBuffer == buff_size - 1)
-        {
-            write(1, output, iForBuffer);
-            iForBuffer = 0, output[iForBuffer] = '\0';
-        }
-        output[iForBuffer] = ch[iForInput];
-        ++outputLength, ++iForBuffer, ++iForInput, output[iForBuffer] = '\0';
-    }
-    return (outputLength);
+	static int iForBuffer = 0;
+	int iForInput = 0;
+	static char output[buff_size];
+	static int outputLength = 0;
+	if (a == -1)
+	{
+		write(1, output, iForBuffer);
+		iForBuffer = 0;
+		return (outputLength);
+	}
+	if (a == 1)
+		output[iForBuffer] = *ch, ++outputLength, ++iForBuffer, ++iForInput, --a, output[iForBuffer] = '\0';
+	while (a--)
+	{
+		if (iForBuffer == buff_size - 1)
+		{
+			write(1, output, iForBuffer);
+			iForBuffer = 0, output[iForBuffer] = '\0';
+		}
+		output[iForBuffer] = ch[iForInput];
+		++outputLength, ++iForBuffer, ++iForInput, output[iForBuffer] = '\0';
+	}
+	return (outputLength);
 }
--- main.h
+++ main.h
@@ -41,17 +41,17 @@ struct placeHolders
 };
 
 placeHolders specifiers[] =
-	{
-		{'c', printChar},
-		{'s', printString},
-		{'d', printInteger},
-		{'i', printInteger},
-		{'b', printBinary},
-		{'u', printUnsigned},
-		{'o', printOctal},
-		{'x', printHex},
-		{'X', printHEX},
-		{'S', printVisibleString},
+{
+	{'c', printChar},
+	{'s', printString},
+	{'d', printInteger},
+	{'i', printInteger},
+	{'b', printBinary},
+	{'u', printUnsigned},
+	{'o', printOctal},
+	{'x', printHex},
+	{'X', printHEX},
+	{'S', printVisibleString},
 };
 
 /*0 task funcs*/
