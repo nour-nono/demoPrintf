fc32fe5ca69f8e9364c63018697e4e143f54fc0b
nono <nour98nour@gmail.com>
2023-09-19 02:00:16 +0300

    betty for specifires.c

--- specifiers.c
+++ specifiers.c
@@ -2,22 +2,23 @@
 
 void searchInSpecfires(char format, int *flag, va_list ptr)
 {
-    placeHolders specifiers[] = {
-        {'c', printChar},
-        {'s', printString},
-        {'d', printInteger},
-        {'i', printInteger},
-        {'b', printBinary},
-        {'u', printUnsigned},
-        {'o', printOctal},
-        {'x', printHex},
-        {'X', printHEX},
-        {'S', printVisibleString},
-    };
-    int j = 0;
-    for (; j < 10; ++j)
-    {
-        if (format == specifiers[j].sp && *flag)
-            specifiers[j].funcPtr(ptr), *flag = 0;
-    }
+	placeHolders specifiers[] = {
+		{'c', printChar},
+		{'s', printString},
+		{'d', printInteger},
+		{'i', printInteger},
+		{'b', printBinary},
+		{'u', printUnsigned},
+		{'o', printOctal},
+		{'x', printHex},
+		{'X', printHEX},
+		{'S', printVisibleString},
+	};
+	int j = 0;
+
+	for (; j < 10; ++j)
+	{
+		if (format == specifiers[j].sp && *flag)
+			specifiers[j].funcPtr(ptr), *flag = 0;
+	}
 }
